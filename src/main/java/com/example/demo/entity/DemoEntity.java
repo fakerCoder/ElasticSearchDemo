af90dff3d1a8061bfce9f093431957b12e8b7c11
lu <lujiang@beeplay123.com>
2019-12-03 21:06:05 +0800

    例子1

--- src/main/java/com/example/demo/entity/DemoEntity.java
+++ src/main/java/com/example/demo/entity/DemoEntity.java
@@ -2,19 +2,37 @@ package com.example.demo.entity;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 
 /**
  * 加上了@Document注解之后，默认情况下这个实体中所有的属性都会被建立索引、并且分词
+ * indexName：对应索引库名称
+ * type：对应在索引库中的类型
+ * shards：分片数量，默认5
+ * replicas：副本数量，默认1
+ * @Id :作用在成员变量，标记一个字段作为id主键
+ *
+ * @Field 作用在成员变量，标记为文档的字段，并指定字段映射属性：
+ * type：字段类型，是是枚举：FieldType
+ * index：是否索引，布尔类型，默认是true
+ * store：是否存储，布尔类型，默认是false
+ * analyzer：分词器名称
+ *
+ik_max_word 会将文本做最细粒度的拆分。
+ik_smart 会做最粗粒度的拆分
  */
-@Document(indexName = "demoname", type = "demo")
+@Document(indexName = "demoname", type = "demo", shards = 1, replicas = 0)
 public class DemoEntity {
+
     @Id
     private String id;
     private String title;
     private String name;
     private String content;
     private String desc;
+    @Field(searchAnalyzer="ik_smart",analyzer="ik_smart",type=FieldType.Integer)
     private Integer age;
 
     @Override
